0963dc9f1263619fd2a0efa27e94163f0099aeb5
gDawg <gonzydesigns@gmail.com>
2018-12-17 23:17:50 -0600

    Added browserify to build

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,32 @@ const { dest, src, task, series } = require('gulp');
 const babel = require('gulp-babel');
 const babelify = require('babelify');
 const browserify = require('browserify');
+const source = require('vinyl-source-stream')
 
-const project = 'MaxiAdvanced';
+const project = 'GonzyPortfolio';
+
+function browserifier () {
+
+	return browserify(`../${project}/app.js`).bundle()
+			.pipe(source('app.js'))
+			.pipe(dest(`../${project}/`));
+
+}
 
 function babler() {
 
-	return src(`../${project}/src/js/history.jsx`)
+	return src(`../${project}/src/js/app.jsx`)
 		.pipe(babel({
 			presets: ['@babel/react']
 		}))
 		.pipe(babel({
 			presets: ['@babel/env']
 		}))
+		//.pipe(browserified)
 		.pipe(dest(`../${project}/`));
-	
+
 }
 
 exports.default = task(babler);
+exports.browserifier = task(browserifier);
+
