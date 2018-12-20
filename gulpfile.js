a1def78df140213bf7f3e003afb99ad322fa1e3a
gDawg <gonzydesigns@gmail.com>
2018-12-20 00:04:21 -0600

    Added SCSS transpiling to gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,20 @@ const { dest, src, task, series } = require('gulp');
 const babel = require('gulp-babel');
 const babelify = require('babelify');
 const browserify = require('browserify');
-const source = require('vinyl-source-stream')
+const rename = require("gulp-rename");
+const source = require('vinyl-source-stream');
+const sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
 
 const project = 'MaxiAdvanced';
-const targetFile = 'serverReact';
+const targetFile = 'history';
 
 //Use builder for front-end, and babler for back-end
 
 function browserifier () {
 
-	return browserify(`../${project}/${targetFile}.js`).bundle()
+	return browserify([`../${project}/${targetFile}.js`]).bundle()
 			.pipe(source(`${targetFile}.js`))
 			.pipe(dest(`../${project}/`));
 
@@ -19,18 +23,29 @@ function browserifier () {
 
 function babler() {
 
+	console.log(`Transpiling ${targetFile} in ${project}`);
 	return src(`../${project}/src/js/${targetFile}.jsx`)
-		.pipe(babel({
-			presets: ['@babel/react']
-		}))
-		.pipe(babel({
-			presets: ['@babel/env']
-		}))
+	.pipe(babel({
+		presets: ['@babel/react']
+	}))
+	.pipe(babel({
+		presets: ['@babel/env']
+	}))
+	.pipe(dest(`../${project}/`));
+
+}
+
+function styles () {
+
+	return src(`../${project}/src/scss/main.scss`)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(rename('app.css'))
 		.pipe(dest(`../${project}/`));
 
 }
 
+exports.styles = task(styles);
 exports.babler = task(babler);
 exports.browserifier = task(browserifier);
-exports.builder = series(babler, browserifier);
+exports.builder = series(styles, babler, browserifier);
 
